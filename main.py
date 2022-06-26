d34a894546e168fd52029229859fb8b96e5c7289
Julien <julien.mousqueton@computacenter.com>
2022-06-26 18:11:51 +0200

    debug

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ from spiders.CertFrSpider import CertFrSpider
 from scrapy.crawler import CrawlerProcess
 from datetime import datetime
 
-
 def main():
     now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
     item = f"""## CyberOwl \n> Last Updated {now} \n\n
 A daily updated summary of the most frequent types of security incidents currently being reported from different sources.\n\n"""
-
 
     with open("docs/README.md", "w") as f:
         f.write(item)
@@ -17,13 +15,12 @@ A daily updated summary of the most frequent types of security incidents current
 
     try:
         process = CrawlerProcess()
-        process.crawl(CisaSpider)
         process.crawl(CertFrSpider)
+        process.crawl(CisaSpider)
         process.start()
 
     except Exception:
         raise ValueError("Error in the spiders!")
-
 
 if __name__ == "__main__":
     main()
